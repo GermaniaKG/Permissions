83413d9a0020f3de25e8a269ceb45eddd9628e4e
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2018-07-09 15:36:53 +0200

    Complete test

--- tests/mysql/PdoAllPermissionsTest.php
+++ tests/mysql/PdoAllPermissionsTest.php
@@ -2,8 +2,11 @@
 namespace mysql;
 
 use Germania\Permissions\PdoAllPermissions;
+use Germania\Permissions\PermissionNotFoundException;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 
 class PdoAllPermissionsTest extends DatabaseTestCaseAbstract
@@ -29,6 +32,25 @@ class PdoAllPermissionsTest extends DatabaseTestCaseAbstract
 
         $this->assertInstanceOf( \Countable::class, ($sut) );
         $this->assertInstanceOf( \Traversable::class, $sut );
+        $this->assertInstanceOf( ContainerInterface::class, $sut );
+
+    }
+
+
+    public function testContainerInterface(  )
+    {
+
+        $pdo = $this->getPdo();
+
+        $sut = new PdoAllPermissions( $pdo, "permissions", "permissions_roles", $this->logger);
+
+        $this->assertInternalType("bool", $sut->has("foo"));
+        $this->assertNotEmpty($sut->get("foo"));
+
+        $this->assertFalse($sut->has("does-not-exist"));
+        $this->expectException( PermissionNotFoundException::class );
+        $this->expectException( NotFoundExceptionInterface::class );
+        $sut->get("does-not-exist");
 
     }
 
